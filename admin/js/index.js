ee99ea29fef07b42a1f002e5bdecb3cbc7bee20b
Ai_ll <yuan85484187@163.com>
2020-08-15 11:47:29 +0800

    子导航完成

--- admin/js/index.js
+++ admin/js/index.js
@@ -1,5 +1,15 @@
 $(function () {
     $('.menu>.level01').click(function () {
         $(this).addClass('active').siblings('.level01').removeClass('active');
-    })
+        if ($(this).index()==1) {
+            $('.level02').slideToggle();
+            $(this).find('b').toggleClass("rotate0")
+        }
+    });
+    $('.level02>li').click(function () {
+        $(this).addClass("active").siblings('li').removeClass('active');
+        
+    });
+
+
 })
